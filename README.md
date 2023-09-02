7b1e8ade01b56994206d32999b4776f8a7077516
Haxaru <sammyalanis43@gmail.com>
2023-09-01 18:57:54 -0700

    Add ui and clean code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # calculator
+
+I think this was one of the harder projects to do. Something I struggled with was understanding how to store the variables and how to call the operate function
+
+I didn't understand why I needed to include the parameters in the called function, but I realize it now. Overall this was a nice project to do. I learned a lot from it.
+
+A weakness I realized I had is understanding parameters and arguments, along with simplifying my code and keeping it minimal.
+
+Something I am really struggling with right now is handling multiple operators.
--- scripts.js
+++ scripts.js
@@ -1,54 +1,93 @@
-let num1, operator, num2;
-
 const addBtn = document.querySelector(".add");
 const clearBtn = document.querySelector(".clear");
 const display = document.querySelector(".display");
 const divideBtn = document.querySelector(".divide");
 const enterBtn = document.querySelector(".enter");
 const multiplyBtn = document.querySelector(".multiply");
-const numbers = document.querySelector(".numbers");
+const numbers = document.querySelectorAll(".num");
 const subtractBtn = document.querySelector(".subtract");
 
-const displayValue = display.textContent;
+let total = 0;
+let num1 = "";
+let operator = "";
+let num2 = "";
 
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
+}
+function clear() {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  display.textContent = "";
+}
+
+function handleOperatorClick(clickedOperator) {
+  num1 = Number(display.textContent);
+  operator = clickedOperator;
+  display.textContent = "";
 }
 
 function operate(num1, operator, num2) {
   switch (operator) {
     case "+":
-      return add(num1, num2);
+      clear();
+      display.textContent = add(num1, num2);
+      break;
     case "-":
-      return subtract(num1, num2);
+      clear();
+      display.textContent = subtract(num1, num2);
+      break;
     case "*":
-      return multiply(num1, num2);
+      clear();
+      display.textContent = multiply(num1, num2);
+      break;
     case "/":
-      return divide(num1, num2);
+      clear();
+      display.textContent = divide(num1, num2);
+      break;
   }
 }
 
-numbers.addEventListener("click", (e) => {
-  display.textContent += e.target.textContent;
-});
+numbers.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    display.textContent += e.target.textContent;
+  })
+);
 
 clearBtn.addEventListener("click", () => {
-  display.textContent = "";
-  operator = undefined;
+  clear();
+  operator = "";
 });
-addBtn.addEventListener("click", () => (operator = "+"));
-subtractBtn.addEventListener("click", () => (operator = "-"));
-multiplyBtn.addEventListener("click", () => (operator = "*"));
-divideBtn.addEventListener("click", () => (operator = "/"));
+addBtn.addEventListener("click", () => {
+  handleOperatorClick("+");
+});
+subtractBtn.addEventListener("click", () => {
+  handleOperatorClick("-");
+});
+multiplyBtn.addEventListener("click", () => {
+  handleOperatorClick("*");
+});
+divideBtn.addEventListener("click", () => {
+  handleOperatorClick("/");
+});
+
+enterBtn.addEventListener("click", () => {
+  num2 = Number(display.textContent);
+  operate(num1, operator, num2);
+});
+
+window.addEventListener("click", () => {
+  console.log(
+    `num1: ${num1}, operator: ${operator}, num2: ${num2} || ${typeof num1}, ${typeof operator}, ${typeof num2}`
+  );
+});
